1117a0dc892cb813d51b300b5852d5370c627aea
Clovn <haliullin101@mail.ru>
2026-01-07 09:02:33 +0300

    update Constants.kt

--- core/util/src/main/java/com/example/util/Constants.kt
+++ core/util/src/main/java/com/example/util/Constants.kt
@@ -2,5 +2,6 @@ package com.example.util
 
 object Constants {
     const val API_URL = "https://dataservice.accuweather.com/"
+    val API_KEY: String? = System.getenv("API_KEY")
     const val LANGUAGE = "ru-ru"
 }
