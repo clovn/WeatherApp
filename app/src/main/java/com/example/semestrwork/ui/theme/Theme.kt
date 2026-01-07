3012b1d42797d31af62408b2b897acad08c8b951
Clovn <haliullin101@mail.ru>
2026-01-07 09:36:32 +0300

    fix detekt issues

--- app/src/main/java/com/example/semestrwork/ui/theme/Theme.kt
+++ app/src/main/java/com/example/semestrwork/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun SemestrWorkTheme(
+fun semestrWorkTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
